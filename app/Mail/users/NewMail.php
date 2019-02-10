067128f5b24d04ffe850052334fbfcc230214c86
eduardo <ecmattos@ig.com.br>
2019-02-10 21:42:59 -0200

    ClientNewMail

--- app/Mail/users/NewMail.php
+++ app/Mail/users/NewMail.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class ClientNewMail extends Mailable
+class ClientNewMail1 extends Mailable
 {
     use Queueable, SerializesModels;
 
