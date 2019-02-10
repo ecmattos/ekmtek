5ab7c18a80149b68eebc6ef3aad5da4880d5a456
eduardo <ecmattos@ig.com.br>
2019-02-10 21:08:18 -0200

    first commit

---
+++ app/Mail/users/NewMail.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class ClientNewMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public $subject;
+    public $client;
+    public $client;
+
+    public function __construct($subject, $client, $user)
+    {
+        $this->subject = $subject;
+        $this->client = $client;
+        $this->client = $user;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('emails.users.new');
+    }
+}
